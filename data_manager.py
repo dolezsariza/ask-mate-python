13abd7247c26aec7ff1bb913ea8225ab6daf7c6b
Gyula Hódosi <gyula.g.hodosi@gmail.com>
2019-06-05 13:50:43 +0200

    added current time for question

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from time import time
 import connection
 
 
 def add_new_question(request_form):
-    new_question = {'id': generate_new_id(), 'submission_time': 1493368154, 'view_number': 0, 'vote_number': 0,
+    new_question = {'id': generate_new_id(), 'submission_time': int(time()+7200), 'view_number': 0, 'vote_number': 0,
                     'title': str(request_form['title']), 'message': str(request_form['message']), 'image': None}
     return connection.append_file("sample_data/question.csv", new_question, connection.QUESTION_HEADERS)
     # data_list is the submit dictionary
