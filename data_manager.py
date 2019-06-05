79279525f92d031d4578e1757d9eb7a8db023746
Attila Boros <boros.attila17@gmail.com>
2019-06-05 13:22:40 +0200

    completed add_new_question

--- data_manager.py
+++ data_manager.py
@@ -2,8 +2,10 @@ from datetime import datetime
 import connection
 
 
-def add_new_question(data_list):
-    return connection.append_file("sample_data/question.csv", data_list, connection.QUESTION_HEADERS)
+def add_new_question(request_form):
+    new_question = {'id': 12, 'submission_time': 1493368154, 'view_number': 0, 'vote_number': 0,
+                    'title': str(request_form['title']), 'message': str(request_form['message']), 'image': None}
+    return connection.append_file("sample_data/question.csv", new_question, connection.QUESTION_HEADERS)
     # data_list is the submit dictionary
 
 
